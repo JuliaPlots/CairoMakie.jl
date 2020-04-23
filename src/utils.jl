eb9a05aeb0330e635cda1258e83b370bf8dd9e07
Anshul Singhvi <asinghvi17@simons-rock.edu>
2020-04-23 15:49:53 +0530

    Add mesh and color utils to utils

--- src/utils.jl
+++ src/utils.jl
@@ -30,3 +30,115 @@ function project_rect(scene, rect::Rect, model)
     maxi = project_position(scene, maximum(rect), model)
     return Rect(mini, maxi .- mini)
 end
+
+################################################################################
+#                                Color handling                                #
+################################################################################
+
+function rgbatuple(c::Colorant)
+    rgba = RGBA(c)
+    red(rgba), green(rgba), blue(rgba), alpha(rgba)
+end
+
+rgbatuple(c) = rgbatuple(to_color(c))
+
+
+function numbers_to_colors(numbers::AbstractArray{<:Number}, primitive)
+
+    colormap = get(primitive, :colormap, nothing) |> to_value |> to_colormap
+    colorrange = get(primitive, :colorrange, nothing) |> to_value
+
+    if colorrange === AbstractPlotting.automatic
+        colorrange = extrema(numbers)
+    end
+
+    AbstractPlotting.interpolated_getindex.(
+        Ref(colormap),
+        Float64.(numbers), # ints don't work in interpolated_getindex
+        Ref(colorrange))
+end
+
+to_uint32_color(c) = reinterpret(UInt32, convert(ARGB32, c))
+
+########################################
+#            Image handling            #
+########################################
+
+function to_cairo_image(img::Matrix{UInt32}, attributes)
+    CairoARGBSurface(
+        [
+            img[j, i]
+            for i in size(img, 2):-1:1, # account for Y-axis discrepancy in Cairo
+                j in 1:size(img, 1)
+        ]
+    )
+end
+
+function to_cairo_image(img, attributes)
+    to_cairo_image(to_uint32_color.(img), attributes)
+end
+
+
+function to_cairo_image(img::AbstractMatrix{<: AbstractFloat}, attributes)
+    imui32 = to_uint32_color.(numbers_to_colors(img, attributes))
+    to_cairo_image(imui32, attributes)
+end
+
+################################################################################
+#                           Mesh handling for Cairo                            #
+################################################################################
+
+struct FaceIterator{Iteration, T, F, ET} <: AbstractVector{ET}
+    data::T
+    faces::F
+end
+
+function (::Type{FaceIterator{Typ}})(data::T, faces::F) where {Typ, T, F}
+    FaceIterator{Typ, T, F}(data, faces)
+end
+function (::Type{FaceIterator{Typ, T, F}})(data::AbstractVector, faces::F) where {Typ, F, T}
+    FaceIterator{Typ, T, F, NTuple{3, eltype(data)}}(data, faces)
+end
+function (::Type{FaceIterator{Typ, T, F}})(data::T, faces::F) where {Typ, T, F}
+    FaceIterator{Typ, T, F, NTuple{3, T}}(data, faces)
+end
+function FaceIterator(data::AbstractVector, faces)
+    if length(data) == length(faces)
+        FaceIterator{:PerFace}(data, faces)
+    else
+        FaceIterator{:PerVert}(data, faces)
+    end
+end
+
+
+Base.size(fi::FaceIterator) = size(fi.faces)
+Base.getindex(fi::FaceIterator{:PerFace}, i::Integer) = fi.data[i]
+Base.getindex(fi::FaceIterator{:PerVert}, i::Integer) = fi.data[fi.faces[i]]
+Base.getindex(fi::FaceIterator{:Const}, i::Integer) = ntuple(i-> fi.data, 3)
+
+function per_face_colors(color, colormap, colorrange, vertices, faces, uv)
+    if color isa Colorant
+        return FaceIterator{:Const}(color, faces)
+    elseif color isa AbstractArray
+        if color isa AbstractVector{<: Colorant}
+            return FaceIterator(color, faces)
+        elseif color isa AbstractVector{<: Number}
+            cvec = AbstractPlotting.interpolated_getindex.((colormap,), color, (colorrange,))
+            return FaceIterator(cvec, faces)
+        elseif color isa AbstractMatrix{<: Colorant} && uv !== nothing
+            cvec = map(uv) do uv
+                wsize = reverse(size(color))
+                wh = wsize .- 1
+                x, y = round.(Int, Tuple(uv) .* wh) .+ 1
+                return color[size(color, 1) - (y - 1), x]
+            end
+            # TODO This is wrong and doesn't actually interpolate
+            # Inside the triangle sampling the color image
+            return FaceIterator(cvec, faces)
+        end
+    end
+    error("Unsupported Color type: $(typeof(color))")
+end
+
+mesh_pattern_set_corner_color(pattern, id, c) =
+    Cairo.mesh_pattern_set_corner_color_rgba(pattern, id, rgbatuple(c)...)
